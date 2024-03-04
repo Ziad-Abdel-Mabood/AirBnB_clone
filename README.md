a49f44b2df2d19fd623eea50316dfbb637e2293e
ziad <ziadabdelmabood@gmail.com>
2024-03-05 00:45:19 +0200

    AirBnB project, write models/base_models.py

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-ALX SWE group project: 0x00. AirBnB clone - The console
+# ALX SWE group project: 0x00. AirBnB clone - The console
+
+a command interpreter to manage AirBnb objects.
+
+
+**examples:**
+
+interactive mode:
+```
+$ ./console.py
+(hbnb) help
+
+Documented commands (type help <topic>):
+========================================
+EOF  help  quit
+
+(hbnb) 
+(hbnb) 
+(hbnb) quit
+$
+```
+
+non-interactive mode:
+```
+$ echo "help" | ./console.py
+(hbnb)
+
+Documented commands (type help <topic>):
+========================================
+EOF  help  quit
+(hbnb) 
+$
+$ cat test_help
+help
+$
+$ cat test_help | ./console.py
+(hbnb)
+
+Documented commands (type help <topic>):
+========================================
+EOF  help  quit
+(hbnb) 
+$
+```
---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" the base class; defines all common attr/methods for other classes """
+import datetime
+import uuid
+
+
+class BaseModel:
+    """ BaseModel class which will pass on common attr/methods for all
+        other classes in the project.
+
+        Attributes:
+            id:             unique id for each instance.
+            created_at:     time and date of creating the instance.
+            updated_at:     time and date of the last change applied
+                            to the instance.
+    """
+
+    def __init__(self):
+        """ constructor method """
+        self.id = uuid.uuid4()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """ string representation of the object """
+        return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """ updates the public instance attribute
+            updated_at with the current datetime """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values
+            of __dict__ of the instance """
+        obj_dict = self.__dict__
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
