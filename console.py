b4110cb8c9ace532322ffa680e80f38b13e2e498
Ziad-Abdel-Mabood <ziadabdlemabood@gmail.com>
2024-03-09 13:29:48 +0200

    [AirBnB_clone], task 6, and implemented create from task 7

---
+++ console.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" entry point of the command interpreter """
+import cmd
+from models.base_model import BaseModel
+
+class HBNBCommand(cmd.Cmd):
+    """ """
+
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        print(end="")
+
+    def do_quit(self, *arg):
+        """
+        exits the command interpreter
+        """
+        return True
+
+    do_EOF = do_quit
+
+    def do_create(self, *arg):
+        """
+        Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id.
+        
+        Usage:
+            create BaseModel
+        """
+        print(arg)
+        if (arg[0] == ''):
+            print("** class name missing **")
+        elif (arg[0] == 'BaseModel'):
+            new_obj = BaseModel()
+            new_obj.save()
+            print(new_obj.id)
+        else:
+            print("** class doesn't exist **")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" User class module """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User class based on BaseModel.
+
+        Attributes:
+
+        Methods:
+    """
